61bd078b9dbd58b5e6a0fb134c85567cdca9dd4c
Craig <craig.frey@hotmail.com>
2013-08-23 20:42:42 -0500

    first commit

---
+++ mrpr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mrpr::Application.config.secret_key_base = 'afe3215757aee4661e4cd973fb8508fac22652eeeb7ef34a86bb88ebdeed06ffdd4130aefb8432bdd237abfd9dd864cc18091cd161d65b35a3d236109469a3b2'
